5d9764d6b77ae8e56dee27a09b0e295005610513
Kent Bull <kent@kentbull.com>
2025-10-25 19:08:37 -0600

    ng19 core module

--- src/app/core/containers/about-vici/about-vici.component.ts
+++ src/app/core/containers/about-vici/about-vici.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-about-vici',
   templateUrl: './about-vici.component.html',
-  styleUrls: ['./about-vici.component.scss']
+  styleUrls: ['./about-vici.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
 })
 export class AboutViciComponent implements OnInit {
 
